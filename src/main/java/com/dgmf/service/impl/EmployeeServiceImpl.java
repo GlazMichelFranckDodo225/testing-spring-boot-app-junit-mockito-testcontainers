2494c9a05cc6053c42551f11eac3b34750a63d41
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-11-22 08:47:20 +0100

    Implementing saveEmployee() method in the Service Layer

--- src/main/java/com/dgmf/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/dgmf/service/impl/EmployeeServiceImpl.java
@@ -1,10 +1,13 @@
 package com.dgmf.service.impl;
 
 import com.dgmf.entity.Employee;
+import com.dgmf.exception.ResourceNotFoundException;
 import com.dgmf.repository.EmployeeRepository;
 import com.dgmf.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -13,6 +16,20 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee saveEmployee(Employee employee) {
-        return employeeRepository.save(employee);
+        // Find Requested Employee By Email
+        Optional<Employee> existingEmployee = employeeRepository
+                .findEmployeeByEmail(employee.getEmail());
+
+        // Check if the Requested Employee already exist
+        if(existingEmployee.isPresent()) {
+            throw new
+                    ResourceNotFoundException("Employee already exist " +
+                    "with given email : " + employee.getEmail());
+        }
+
+        // Save Requested Employee
+        Employee savedEmployee = employeeRepository.save(employee);
+
+        return savedEmployee;
     }
 }
